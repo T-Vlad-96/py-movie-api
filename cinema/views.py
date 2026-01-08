c72da19474f98fce948910a22c52b5a55d19affe
user <user@MacBook-Air-user.local>
2026-01-08 16:26:03 +0200

    chore: re-format the code according to flake8 standarts

--- cinema/views.py
+++ cinema/views.py
@@ -11,10 +11,39 @@ def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
+        return Response(
+            serializer.data, status.HTTP_200_OK
+        )
     else:
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+            return Response(
+                serializer.data, status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request, pk):
+    movie = Movie.objects.get(id=pk)
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(
+            serializer.data, status=status.HTTP_200_OK
+        )
+    elif request.method == "PUT":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(
+                serializer.data, status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+    else:
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
